d80c686c471d5c52a61eb58edafc5bd8133b008e
MukeshCheekatla <cheekatlamukesh@gmail.com>
2026-04-05 13:58:53 +0530

    extension: remove unreachable legacy files

--- extension/src/types/globals.d.ts
+++ extension/src/types/globals.d.ts
@@ -1,12 +1,7 @@
 interface Window {
   __dashPulseChart?: any;
   __dashStorageListener?: (changes: any) => void;
-  __dashTicker?: any;
-  __dashUsageSync?: any;
-  __dashTimer?: any;
   __focusInterval?: any;
-  __focusPopupRenderTimer?: any;
-  __focusRenderTimer?: any;
 }
 
 declare var Chart: any;
